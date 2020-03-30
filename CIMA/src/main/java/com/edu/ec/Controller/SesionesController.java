e7e7d763d3bc28b2a48f9e2df626075098d480eb
John Llanes <jlc_rb@live.com>
2020-03-30 01:34:10 -0500

    Para controlar las sesiones

---
+++ CIMA/src/main/java/com/edu/ec/Controller/SesionesController.java
@@ -0,0 +1,14 @@
+package com.edu.ec.Controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import com.edu.ec.Favorito.UsuarioFavorito;
+
+@ManagedBean
+public class SesionesController {
+
+	@Inject
+	private UsuarioFavorito usuraiofav;
+	
+}
